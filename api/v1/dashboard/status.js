95fea3cb1ba3febaf9e77d1c6d61a1adc950525d
Ronald Retraubun <ronaldretraubun2022@gmail.com>
2026-06-13 04:21:14 +0900

    fix vercel dashboard status import

--- api/v1/dashboard/status.js
+++ api/v1/dashboard/status.js
@@ -1,5 +1,18 @@
-const { createDashboardResponse, sendJson } = require("../dashboard.js");
+cat > (api / v1 / dashboard / status.js) << "EOF";
+const { createDashboardResponse, sendJson } = require("../dashboard");
 
 module.exports = function handler(req, res) {
-  sendJson(res, createDashboardResponse());
+  if (req.method && req.method !== "GET") {
+    return sendJson(
+      res,
+      {
+        success: false,
+        message: "Method not allowed.",
+      },
+      405,
+    );
+  }
+
+  return sendJson(res, createDashboardResponse());
 };
+EOF;
